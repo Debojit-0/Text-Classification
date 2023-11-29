04bcfdabe188e88d56628decbbf35000c8b85e8b
Debojit Mandal <77208481+Debojit-0@users.noreply.github.com>
2023-11-29 12:20:35 +0530

    Add files via upload

--- src/Textclassification/entity/__init__.py
+++ src/Textclassification/entity/__init__.py
@@ -17,6 +17,13 @@ class DataCleaningConfig:
     data_files:  Path
 
 @dataclass(frozen=True)
+class DataValidationConfig:
+    root_dir: Path
+    STATUS_FILE: str
+    ALL_REQUIRED_FILES: list
+
+
+@dataclass(frozen=True)
 class ModelTrainerConfig:
     root_dir: Path
     data_path: Path
